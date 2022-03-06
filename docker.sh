67105a11787b618d5c01dd6a21a5b72d399f1891
sotupercebes <90618162+sotupercebes@users.noreply.github.com>
2022-03-06 16:18:02 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -38,3 +38,6 @@ echo 'Server is updated... Moving on'
 pause
 ###################################UPDATE FINISHED################################################
 snap install docker
+docker volume create portainer_data
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+pause
